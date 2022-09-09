3e4a539144929dcd6e6d433c376dc0d625aa0ed2
ShahabAthar25 <shahabathar25@gmail.com>
2022-09-09 15:55:06 +0500

    Add left side of home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,13 +6,16 @@ import {
   Bars4Icon,
   Cog8ToothIcon,
   FlagIcon,
+  PencilSquareIcon,
   QuestionMarkCircleIcon,
+  VideoCameraIcon,
 } from "@heroicons/react/24/outline/";
+import { useState } from "react";
 
 export default function Home() {
   const { isAuthenticated, user } = useAuth0();
 
-  console.log(user.picture);
+  const [link, setLink] = useState("");
 
   return (
     <div className="w-screen h-screen flex flex-col">
@@ -44,8 +47,54 @@ export default function Home() {
           )}
         </div>
       </div>
-      <div className="flex-[90%]">
-        <h1>Hello World</h1>
+      <div className="flex-[90%] flex m-auto w-full">
+        <div className="flex-[50%] ml-24 flex flex-col justify-center">
+          <div>
+            <div className="text-[2.75rem] font-medium leading-[3.25rem] tracking-normal font-roboto text-gray-900 max-w-[500px]">
+              <h1>Premium video meetings.</h1>
+              <h1>Now free for everyone.</h1>
+              <p className="text-gray-600 text-base mt-6 mb-12">
+                We re-engineered the service we built for secure business
+                meetings, Google Meet, to make it free and available for all.
+              </p>
+            </div>
+            <div className="flex items-center space-x-7">
+              <button className="bg-blue-600 flex items-center rounded-lg px-3 py-3 border-0 text-white font-roboto font-semibold">
+                <VideoCameraIcon className="h-6 text-white mr-4" />
+                New meeting
+              </button>
+              <form className="flex items-center space-x-4">
+                <div className="flex items-center border-[1px] border-gray-500 rounded-sm px-2">
+                  <PencilSquareIcon className="h-6 text-gray-600" />
+                  <input
+                    type="text"
+                    className="bg-transparent border-none px-2 py-3 outline-none"
+                    value={link}
+                    onChange={(e) => setLink(e.target.value)}
+                  />
+                </div>
+                {link.length !== 0 && (
+                  <button className="text-blue-600 px-4 py-3 rounded-sm hover:bg-blue-50 duration-300">
+                    Join
+                  </button>
+                )}
+              </form>
+            </div>
+          </div>
+          <div className="border-[1px] border-gray-400 w-full my-8 max-w-xl"></div>
+          <div className="flex font-roboto">
+            <a
+              href="https://support.google.com/meet/?hl=en"
+              className="pr-1 text-blue-600 hover:underline"
+            >
+              Learn more
+            </a>
+            about google meet
+          </div>
+        </div>
+        <div className="flex-[50%] flex items-center justify-center">
+          <h1>Right</h1>
+        </div>
       </div>
     </div>
   );
